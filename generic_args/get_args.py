f272ed5f11922a068e24d8b39fcc826caa3a308c
Anatoly Scherbakov <altaisoft@gmail.com>
2021-02-27 19:06:45 +0700

    Try to fix mypy error on Python 3.7

--- generic_args/get_args.py
+++ generic_args/get_args.py
@@ -1,8 +1,15 @@
-try:
-    # For Python >= 3.8: use the method from standard library.
-    # https://docs.python.org/3/library/typing.html#typing.get_args
-    from typing import get_args
-except ImportError:   # pragma: no cover
-    # For older versions, rely upon typing_inspect.
-    # https://github.com/ilevkivskyi/typing_inspect
-    from typing_inspect import get_args  # type: ignore
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:  # pragma: no cover
+    # This one is always available.
+    from typing_inspect import get_args
+
+else:  # pragma: no cover
+    try:
+        # For Python >= 3.8: use the method from standard library.
+        # https://docs.python.org/3/library/typing.html#typing.get_args
+        from typing import get_args
+    except ImportError:
+        # For older versions, rely upon typing_inspect.
+        # https://github.com/ilevkivskyi/typing_inspect
+        from typing_inspect import get_args
