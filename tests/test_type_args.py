4df6dc8be86cbd6620b0d9a96e895a2d3c1c6d9d
Anatoly Scherbakov <altaisoft@gmail.com>
2021-02-27 18:39:10 +0700

    Improve tests

--- tests/test_type_args.py
+++ tests/test_type_args.py
@@ -1,23 +1,44 @@
-from typing import Generic, TypeVar
+from typing import Tuple, TypeVar, Union
 
 import pytest
 
 from generic_args import TypeArgsError, generic_type_args
 
-Type1 = TypeVar('Type1')
-Type2 = TypeVar('Type2')
+T = TypeVar('T')   # noqa: WPS111
 
 
-@pytest.mark.parametrize(('generic_type', 'type_parameters'), [
-    (Generic[Type1], (Type1, )),
-    (Generic[Type1, Type2], (Type1, Type2)),
-])
-def test_generic(generic_type, type_parameters):
+SUCCESS_TESTCASES = [   # noqa: WPS407
+    # Source: typing_inspect.get_args docstring
+    (
+        Union[int, Union[T, int], str][int],  # type: ignore
+        (int, str),
+    ),
+    (
+        Union[int, Tuple[T, int]][str],  # type: ignore
+        (int, Tuple[str, int]),
+    ),
+]
+
+
+FAILURE_TESTCASES = [   # noqa: WPS407
+    int,
+    str,
+
+    5,
+    3.1415,
+    'foo',
+    {},
+]
+
+
+@pytest.mark.parametrize(('generic_type', 'type_parameters'), SUCCESS_TESTCASES)
+def test_success(generic_type, type_parameters):
     """Tests for typing.Generic."""
     assert generic_type_args(generic_type) == type_parameters
 
 
-def test_dict():
-    """Test just a simple dict."""
+@pytest.mark.parametrize('type_or_value', FAILURE_TESTCASES)
+def test_failure(type_or_value):
+    """Test a value which does not have generic parameters."""
     with pytest.raises(TypeArgsError):
-        generic_type_args({})
+        generic_type_args(type_or_value)
